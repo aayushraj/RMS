8ac43592c79c9b9c7b7c03ca37cdfa17024a9707
aayush <aayushrajopadhyaya@gmail.com>
2022-02-13 17:58:31 +0545

    added utility helper

---
+++ RMS.Repository/Helper/ICommonUtilityRepository.cs
@@ -0,0 +1,100 @@
+﻿using RMS.Models.Common;
+
+namespace RMS.Repository
+{
+    public interface ICommonUtilityRepository
+    {
+        List<DDLModel> GetStates();
+        List<DDLModel> GetDistricts(int stateId);
+        List<DDLModel> GetFloor(int floorIdd);
+        List<DDLModel> GetRelationships();
+        string GetMonth(int id);
+    }
+    public class CommonUtilityRepository : ICommonUtilityRepository
+    {
+        private readonly IDapperService _dapperService;
+        public CommonUtilityRepository(IDapperService dapperService)
+        {
+            _dapperService = dapperService;
+        }
+        public List<DDLModel> GetDistricts(int stateId)
+        {
+            string sql = @"Select DistrictId AS [Value],DistrictName AS [Text] from RMS_INFO_DISTRICT where StateId = @id";
+            return _dapperService.Query<DDLModel>(sql, _dapperService.AddParam(stateId));
+        }
+
+        public List<DDLModel> GetFloor(int floorId)
+        {
+            string sql = @"Select * from RMS_INFO where id=@Id";
+            return _dapperService.Query<DDLModel>(sql, _dapperService.AddParam(floorId));
+        }
+
+        public string GetMonth(int id)
+        {
+            var Month = "a";
+            switch (id)
+            {
+                case 1:
+                    Month = "January";
+                    break;
+
+                case 2:
+                    Month = "February";
+                    break;
+                case 3:
+                    Month = "March";
+                    break;
+                case 4:
+                    Month = "April";
+                    break;
+                case 5:
+                    Month = "May";
+                    break;
+                case 6:
+                    Month = "June";
+                    break;
+                case 7:
+                    Month = "July";
+                    break;
+                case 8:
+                    Month = "August";
+                    break;
+                case 9:
+                    Month = "September";
+                    break;
+                case 10:
+                    Month = "October";
+                    break;
+                case 11:
+                    Month = "November";
+                    break;
+                case 12:
+                    Month = "December";
+                    break;
+            };
+
+            return Month;
+        }
+
+        public List<DDLModel> GetRelationships()
+        {
+            var relationshipList = new List<DDLModel>()
+                {
+                    new DDLModel(){Text = "Mother", Value = "Mother"},
+                    new DDLModel(){Text = "Father", Value = "Father"},
+                    new DDLModel(){Text = "Brother", Value = "Brother"},
+                    new DDLModel(){Text = "Sister", Value = "Sister"},
+                    new DDLModel(){Text = "Daughter", Value = "Daughter"},
+                    new DDLModel(){Text = "Son", Value = "Son"},
+                };
+
+            return relationshipList;
+        }
+
+        public List<DDLModel> GetStates()
+        {
+            string sql = @"SELECT StateId AS Value,StateName AS Text FROM RMS_INFO_STATE";
+            return _dapperService.Query<DDLModel>(sql);
+        }
+    }
+}
---
+++ RMS.Service/Helpers/CommonUtilityService.cs
@@ -0,0 +1,55 @@
+﻿using RMS.Models.Common;
+using RMS.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace RMS.Service.Helpers
+{
+    public interface ICommonUtilityService
+    {
+        List<DDLModel> GetStates();
+        List<DDLModel> GetDistricts(int stateId);
+        List<DDLModel> GetFloor();
+        List<DDLModel> GetRelationship();
+        string GetMonth(int id);
+    }
+    public class CommonUtilityService : ICommonUtilityService
+    {
+        private readonly ICommonUtilityRepository _commonUtilities;
+        public CommonUtilityService(ICommonUtilityRepository commonUtilities)
+        {
+            _commonUtilities = commonUtilities;
+        }
+
+        public List<DDLModel> GetDistricts(int stateId)
+        {
+            return _commonUtilities.GetDistricts(stateId);
+        }
+
+        public List<DDLModel> GetFloor()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetMonth(int id)
+        {
+            return _commonUtilities.GetMonth(id);
+        }
+
+        public List<DDLModel> GetRelationship()
+        {
+            return _commonUtilities.GetRelationships();
+        }
+
+        public List<DDLModel> GetStates()
+        {
+            return _commonUtilities.GetStates();
+        }
+    }
+
+
+}
