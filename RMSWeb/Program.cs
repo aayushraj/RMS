6bbf6800fe5177d9c7b48b0a64048639e4155cfb
aayush <aayushrajopadhyaya@gmail.com>
2022-02-16 23:28:07 +0545

    UI changes. Tables and Icons

--- RMSWeb/Program.cs
+++ RMSWeb/Program.cs
@@ -37,6 +37,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=TenantInfo}/{action=Index}/{id?}");
 
 app.Run();
