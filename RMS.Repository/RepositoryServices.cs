15a2af560d90eb45d7d20ec2a5dfd29586ff117a
Nitish Rajopadhyaya <nitishrocker220@gmailcom>
2022-05-01 09:15:36 +0545

    Di included

--- RMS.Repository/RepositoryServices.cs
+++ RMS.Repository/RepositoryServices.cs
@@ -3,6 +3,7 @@ using RMS.Repository.FamilyInfo;
 //using RMS.Repository.Payment;
 using RMS.Repository.Report;
 using RMS.Repository.TenantInfo;
+using RMS.Repository.Payment;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,9 +18,9 @@ namespace RMS.Repository
         {
             ser.AddTransient<IDapperService, DapperService>();
             ser.AddScoped<ICommonUtilityRepository, CommonUtilityRepository>();
-            //ser.AddScoped<IPaymentRepository,PaymentRepository>();
+            ser.AddScoped<IPaymentRepository,PaymentRepository>();
             ser.AddScoped<IReportRepository,ReportRepository>();
-            //ser.AddScoped<IRentSetupRepository, RentSetupRepository>();
+            ser.AddScoped<IRentSetupRepository, RentSetupRepository>();
             //ser.AddScoped<ISetupRepository, SetupRepository>();
             ser.AddScoped<IFamilyInfoRepository, FamilyInfoRepository>();
             ser.AddScoped<ITenantInfoRepository, TenantInfoRepository>();
