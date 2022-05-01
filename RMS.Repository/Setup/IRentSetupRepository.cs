50f29a8ec7ecef6f3c23850445766825edd1952d
Nitish Rajopadhyaya <nitishrocker220@gmailcom>
2022-05-01 09:12:09 +0545

    Rentsetup Di included

--- RMS.Repository/Setup/IRentSetupRepository.cs
+++ RMS.Repository/Setup/IRentSetupRepository.cs
@@ -1,44 +1,56 @@
-﻿//using RMS.Models;
+﻿using RMS.Models;
 
-//namespace RMS.Repository
-//{
-//    public interface IRentSetupRepository:IGenericRepository<RentSetupModel>
-//    {
-//        RentSetupModel GetByFloorId(int id);
-//    }
-//    public class RentSetupRepository : IRentSetupRepository
-//    {
-//        public bool Create(RentSetupModel model)
-//        {
-//            throw new System.NotImplementedException();
-//        }
+namespace RMS.Repository
+{
+    public interface IRentSetupRepository
+    {
+        RentSetupModel GetByFloorId(int id);
+        bool Create(RentSetupModel model); 
+        bool Delete(RentSetupModel model);
+        bool Edit(RentSetupModel model);
+        RentSetupModel GetById(int? id);
+        List<RentSetupModel> GetList();
 
-//        public bool Delete(RentSetupModel model)
-//        {
-//            throw new System.NotImplementedException();
-//        }
 
-//        public bool Edit(RentSetupModel model)
-//        {
-//            throw new System.NotImplementedException();
-//        }
+    }
+    public class RentSetupRepository : IRentSetupRepository
+    {
+        private readonly IDapperService _dapper;
+        public RentSetupRepository(IDapperService Dapper)
+        {
+            _dapper = Dapper;
+        }
+        public bool Create(RentSetupModel model)
+        {
+            throw new System.NotImplementedException();
+        }
 
-//        public RentSetupModel GetByFloorId(int id)
-//        {
-//            string sql = @"select * from RentSetup where FloorId=@id";
-//            var parameter = DapperService.AddParam(id);
-//            var model = DapperService.Query<RentSetupModel>(sql, parameter).FirstOrDefault();
-//            return model;
-//        }
+        public bool Delete(RentSetupModel model)
+        {
+            throw new System.NotImplementedException();
+        }
 
-//        public RentSetupModel GetById(int? id)
-//        {
-//            throw new System.NotImplementedException();
-//        }
+        public bool Edit(RentSetupModel model)
+        {
+            throw new System.NotImplementedException();
+        }
 
-//        public List<RentSetupModel> GetList()
-//        {
-//            throw new System.NotImplementedException();
-//        }
-//    }
-//}
+        public RentSetupModel GetByFloorId(int id)
+        {
+            string sql = @"select * from RentSetup where FloorId=@id";
+            var parameter = _dapper.AddParam(id);
+            var model = _dapper.Query<RentSetupModel>(sql, parameter).FirstOrDefault();
+            return model;
+        }
+
+        public RentSetupModel GetById(int? id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public List<RentSetupModel> GetList()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
